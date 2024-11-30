3053e11fecc307be9404f58443562d4a79cdb255
mohamed-295 <ma2267204@gmail.com>
2024-11-30 23:37:34 +0200

    Solve B

--- Rookies/Task1/A.cpp
+++ Rookies/Task1/A.cpp
@@ -1,1 +1,24 @@
-//Potato League
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    using namespace std;
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int s = 0;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s += x;
+        if (s < 0)
+            ans = max(ans, -s);
+    }
+    cout << ans << endl;
+    return 0;
+}
